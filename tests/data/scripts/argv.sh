3f9bb74614c2342712b67da2f687fed09cbb50db
Mathias Schindler <3246297+MathiasSchindler@users.noreply.github.com>
2025-12-14 21:26:53 +0100

    Delete tests directory

--- tests/data/scripts/argv.sh
+++
@@ -1,5 +0,0 @@
-# argv.sh: positional parameter sanity
-
-echo argv0=$0
-echo argv1=$1
-echo argv2=$2
--- tests/data/scripts/argv_params.sh
+++
@@ -1,8 +0,0 @@
-# argv_params.sh: positional parameter expansions
-
-echo argv0=$0
-echo argv1=$1
-echo argv2=$2
-echo argc=$#
-echo at=$@
-echo star=$*
